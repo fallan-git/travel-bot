8b6e17d68b3775db608081bb7e6e7d819e66f95d
TsyrulMark <tsyrulmark@gmail.com>
2024-05-22 15:25:14 +0600

    Сделал файл tokens.py c выдачей iam_token, сделал файл config.py

--- config.py
+++ config.py
@@ -1,3 +1,11 @@
-TOKEN = ""
-FOLDER_ID = ""
-IAM_TOKEN = ""
+MAX_USERS = 10
+MAX_GPT_TOKENS = 120
+MAX_USER_GPT_TOKENS = 4000
+
+HOME_DIR = '/home/student/travel-bot'
+DB_FILE = f'{HOME_DIR}/TravelBot.db'
+LOGS = f'{HOME_DIR}/logs.txt'
+IAM_TOKEN_PATH = f'{HOME_DIR}/creds/iam.txt'
+
+
+
